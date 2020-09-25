d5eb81112db6678ca13ead0ea1a2bc81e17ce34b
Kyrylo Shpak <kshpak@redhat.com>
2020-09-25 17:09:54 +0200

    Add. Constructor parameter for User class

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,2 +1,29 @@
-package org.example.model;public class User {
+package org.example.model;
+
+public class User {
+    private String name;
+    private int age;
+
+
+    public User(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
 }
